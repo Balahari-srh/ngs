de414435b1ca0b3e3daafff027f0b3cc0fc568ee
Balahari Vignesh <balaharivignesh.balu@stud.hochschule-heidelberg.de>
2020-04-14 08:31:57 +0200

    Complete challenge 2 with own example

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,16 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+const timerId= setInterval(
+  () => console.log('Hello World'),
+  1000,
+);
+
+for (let i = 0; i < 0.5e0; i++) {
+  // Block Node Synchronously
+
+}
+setTimeout(
+  ()=> clearInterval(timerId),
+  6000,
+);

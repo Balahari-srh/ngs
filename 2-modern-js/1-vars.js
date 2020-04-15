d5253e3579d157738dea046b5a2f700f176ec1ea
Balahari Vignesh <balaharivignesh.balu@stud.hochschule-heidelberg.de>
2020-04-15 03:54:08 +0200

    changed var to let

--- 2-modern-js/1-vars.js
+++ 2-modern-js/1-vars.js
@@ -6,13 +6,14 @@ if (true) {
   // Block Scope
 }
 
-for (var i = 1; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
   // Block Scope
 }
 
 function sum(a, b) {
   // Function Scope
-  var result = a + b;
+  let result = a + b;
+  return result;
 }
 
-sum(4 + 3);
+console.log(sum(4,3));

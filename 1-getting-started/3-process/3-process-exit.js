3fa2a545ad02b80ab151f8265e3e3b43ce1e75ab
Balahari Vignesh <balaharivignesh.balu@stud.hochschule-heidelberg.de>
2020-04-15 03:38:54 +0200

    Process example

--- 1-getting-started/3-process/3-process-exit.js
+++ 1-getting-started/3-process/3-process-exit.js
@@ -1,7 +1,8 @@
-setTimeout(() => process.exit(), 2000);
+setTimeout(() => process.exit(), 4000);
 
 process.on('exit', () => {
   console.log('Process will exit now. See you later!');
 });
 
 console.log('Hello!');
+console.log('Process will exit in 4 seconds.');
